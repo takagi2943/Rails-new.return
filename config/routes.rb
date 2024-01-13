04751ab964bd6a8b6be060aa27fd6fde00ed3a0e
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-13 07:30:23 +0000

    ビューファイルを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create' #ここを追記
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
