9d3b5e9b7ef5a4f00a9bede2f236ce6ea04ecd69
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-13 08:16:10 +0000

    編集フォームを作成【ビューに記述を追加】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,12 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
 
   private
   #ストロングパラメータ
