ea888cfe4565251ae54f576785feea3b38a94054
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-13 07:36:23 +0000

    showアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists#create' #ここを追記
-  get 'lists' => 'lists#index'
-  get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
+  post 'lists' => 'lists#create' #ここを追記
+  get 'lists' => 'lists#index'
+  #.../lists/1や、.../lists/3に該当する
+  get 'lists/:id' => 'lists#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
