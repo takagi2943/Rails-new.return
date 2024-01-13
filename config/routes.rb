67d9a6daf82fba2d3e0c2e351df34588455c8371
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-13 08:06:52 +0000

    編集フォームを作成【ビュー】

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create' #ここを追記
   get 'lists' => 'lists#index'
   #.../lists/1や、.../lists/3に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
-
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
